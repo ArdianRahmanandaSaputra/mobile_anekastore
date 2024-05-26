d6a9814c799a90f9c557c0ede806818dfc0563e7
Ahmad Abu Hasan <36786100+eby8zevin@users.noreply.github.com>
2024-05-26 22:46:48 +0700

    style: remove import not used

--- app/src/main/java/com/example/anekastoremobile/ui/homeui/home/HomeAdapter.kt
+++ app/src/main/java/com/example/anekastoremobile/ui/homeui/home/HomeAdapter.kt
@@ -4,10 +4,8 @@ import android.content.Context
 import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.LinearLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.example.anekastoremobile.data.remote.response.GetProductResponse
 import com.example.anekastoremobile.data.remote.response.Product
 import com.example.anekastoremobile.databinding.ItemProductBinding
 
