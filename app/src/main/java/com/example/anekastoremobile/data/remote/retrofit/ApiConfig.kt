d072da59a64a64eb9bbb81c9a002979ebc70b5eb
Ahmad Abu Hasan <36786100+eby8zevin@users.noreply.github.com>
2024-05-29 23:44:14 +0700

    feat(api): added tokenInterceptor

--- app/src/main/java/com/example/anekastoremobile/data/remote/retrofit/ApiConfig.kt
+++ app/src/main/java/com/example/anekastoremobile/data/remote/retrofit/ApiConfig.kt
@@ -1,24 +1,39 @@
 package com.example.anekastoremobile.data.remote.retrofit
 
-import com.google.gson.GsonBuilder
+import android.content.Context
+import android.content.SharedPreferences
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiConfig {
-    fun getService(): ApiService{
-       val loggingInterceptor = HttpLoggingInterceptor()
-           .setLevel(HttpLoggingInterceptor.Level.BODY)
+    fun getService(context: Context): ApiService {
+
+        val sharedPreferences: SharedPreferences = context.getSharedPreferences("user_pref", Context.MODE_PRIVATE)
+        val token = sharedPreferences.getString("token", "") ?: ""
+
+        val loggingInterceptor = HttpLoggingInterceptor()
+            .setLevel(HttpLoggingInterceptor.Level.BODY)
+
+        val tokenInterceptor = Interceptor { chain ->
+            val request = chain.request().newBuilder()
+                .addHeader("Authorization", "Bearer $token")
+                .build()
+            chain.proceed(request)
+        }
+
         val client: OkHttpClient = OkHttpClient.Builder()
             .addInterceptor(loggingInterceptor)
+            .addInterceptor(tokenInterceptor)
             .build()
 
-        val retofit = Retrofit.Builder()
+        val retrofit = Retrofit.Builder()
             .baseUrl("http://storeaneka.my.id/api/")
             .addConverterFactory(GsonConverterFactory.create())
             .client(client)
             .build()
-        return retofit.create(ApiService::class.java)
+        return retrofit.create(ApiService::class.java)
     }
 }
