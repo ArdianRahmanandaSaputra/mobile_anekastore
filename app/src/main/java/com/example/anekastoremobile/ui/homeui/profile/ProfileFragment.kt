c7a13ad0dc78b7488de5a31d2068ef25dd6bf53d
Ahmad Abu Hasan <36786100+eby8zevin@users.noreply.github.com>
2024-06-03 23:05:07 +0700

    feat(profile): create feature profile

---
+++ app/src/main/java/com/example/anekastoremobile/ui/homeui/profile/ProfileFragment.kt
@@ -0,0 +1,43 @@
+package com.example.anekastoremobile.ui.homeui.profile
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.anekastoremobile.R
+import com.example.anekastoremobile.databinding.FragmentProfileBinding
+
+class ProfileFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = ProfileFragment()
+    }
+
+    private lateinit var viewModel: ProfileViewModel
+    private var _binding : FragmentProfileBinding? = null
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(ProfileViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
