ef35f3757c800f8b65f45de075d7b98987b2b2ae
Ahmad Abu Hasan <36786100+eby8zevin@users.noreply.github.com>
2024-06-04 22:44:08 +0700

    feat(api): config api endpoint profile

--- app/src/main/java/com/example/anekastoremobile/data/remote/retrofit/ApiService.kt
+++ app/src/main/java/com/example/anekastoremobile/data/remote/retrofit/ApiService.kt
@@ -7,6 +7,7 @@ import com.example.anekastoremobile.data.remote.response.GetProductResponse
 import com.example.anekastoremobile.data.remote.response.LoginResponse
 import com.example.anekastoremobile.data.remote.response.MessageResponse
 import com.example.anekastoremobile.data.remote.response.ProductViewResponse
+import com.example.anekastoremobile.data.remote.response.ProfileResponse
 import com.example.anekastoremobile.data.remote.response.RegisterResponse
 import retrofit2.Call
 import retrofit2.http.Body
@@ -59,4 +60,6 @@ interface ApiService {
         @Field("cart_id") cartId: Int,
     ): Call<MessageResponse>
 
+    @GET("profile")
+    fun profile():Call<ProfileResponse>
 }
--- app/src/main/java/com/example/anekastoremobile/ui/homeui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/anekastoremobile/ui/homeui/profile/ProfileFragment.kt
@@ -1,13 +1,20 @@
 package com.example.anekastoremobile.ui.homeui.profile
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.Glide
 import com.example.anekastoremobile.R
+import com.example.anekastoremobile.data.remote.response.ProfileResponse
+import com.example.anekastoremobile.data.remote.retrofit.ApiConfig
 import com.example.anekastoremobile.databinding.FragmentProfileBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class ProfileFragment : Fragment() {
 
@@ -16,7 +23,8 @@ class ProfileFragment : Fragment() {
     }
 
     private lateinit var viewModel: ProfileViewModel
-    private var _binding : FragmentProfileBinding? = null
+    private var _binding: FragmentProfileBinding? = null
+
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
@@ -32,6 +40,58 @@ class ProfileFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        getData()
+    }
+
+    private fun getData() {
+        showLoading(true)
+        val client = ApiConfig.getService(requireContext()).profile()
+        client.enqueue(object : Callback<ProfileResponse> {
+            override fun onResponse(p0: Call<ProfileResponse>, p1: Response<ProfileResponse>) {
+                showLoading(false)
+                val responseBody = p1.body()
+                if (p1.isSuccessful && responseBody != null) {
+                    viewUI(responseBody)
+                }
+            }
+
+            override fun onFailure(p0: Call<ProfileResponse>, p1: Throwable) {
+                showLoading(false)
+                Log.e("ProfileFragment", p1.message.toString())
+            }
+        })
+    }
+
+    private fun viewUI(data: ProfileResponse) {
+        _binding?.profileImage?.let {
+            Glide.with(requireContext())
+                .load(data.detail?.photo)
+                .error(R.drawable.baseline_person_24)
+                .into(it)
+        }
+        _binding?.name?.text = data.user?.name
+        _binding?.email?.text = data.user?.email
+
+        _binding?.nameUser?.text = data.user?.name
+        _binding?.phone?.text = data.detail?.phone
+        _binding?.address?.text =
+            buildString {
+                append(data.detail?.detailAddress)
+                append(", ")
+                append(data.detail?.city)
+                append(", ")
+                append(data.detail?.province)
+                append(" ")
+                append(data.detail?.postalCode)
+            }
+    }
+
+    fun showLoading(isLoading: Boolean) {
+        if (isLoading) {
+            binding.progressBar.visibility = View.VISIBLE
+        } else {
+            binding.progressBar.visibility = View.GONE
+        }
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
